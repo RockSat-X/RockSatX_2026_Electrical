072b663ea0a93b3b7eebb61ef10d9b832a5f22e9
Phuc X. Doan <phucxdoan@gmail.com>
2025-07-25 02:03:14 -0400

    Exporting `MetaError`.

--- electrical/Common.py
+++ electrical/Common.py
@@ -1,9 +1,10 @@
-#meta root, Obj, Record, Table, log, ErrorLift, CMSIS_SET, CMSIS_WRITE, CMSIS_SPINLOCK, TARGETS :
+#meta root, Obj, Record, Table, log, MetaError, ErrorLift, CMSIS_SET, CMSIS_WRITE, CMSIS_SPINLOCK, TARGETS :
 # TODO Provide explaination on how this file works?
 
 import enum
-from deps.pxd.utils import root, coalesce, repr_in_c, find_dupe, ljusts, Obj, Record, Table, OrdSet, ErrorLift
-from deps.pxd.log   import log
+from deps.pxd.utils            import root, coalesce, repr_in_c, find_dupe, ljusts, Obj, Record, Table, OrdSet, ErrorLift
+from deps.pxd.log              import log
+from deps.pxd.metapreprocessor import MetaError
 
 
 
